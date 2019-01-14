eb983512767d2a82ce33d7ca39c17309dbe0b580
OMECON\brzyszkiewicz <brzyszkiewicz@omecon.pl>
2019-01-14 16:17:48 +0100

    szkielety

--- src/main/java/jarOperations/JarModifier.java
+++ src/main/java/jarOperations/JarModifier.java
@@ -1,4 +1,30 @@
 package jarOperations;
 
 public class JarModifier {
+
+    public void addMethod(){
+
+    }
+    public void deleteMethod(){
+
+    }
+    public void deleteConstructor(){
+
+    }
+    public void deleteField(){
+
+    }
+    public void overwriteMethod(){
+
+    }
+    public void overwriteConstructor(){
+
+    }
+    public void insertAtTheEnd(){
+
+    }
+    public void insertInTheBeginnning(){
+
+    }
+
 }
